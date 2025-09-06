9f0286dffee7a7a5f5aba49abb7a51a88b392933
Miguel Oresca <0xelayja@users.noreply.github.com>
2025-09-06 12:36:31 +0800

    Add main soft launch mockup files

---
+++ main-v1.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('fashionModal');
+const closeBtn = document.getElementById('closeModal');
+const triggers = document.querySelectorAll('.popup-trigger');
+
+triggers.forEach(trigger => {
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => modal.style.display = 'none');
+window.addEventListener('click', e => {
+  if (e.target === modal) modal.style.display = 'none';
+});
+
+
+                                                                                                
